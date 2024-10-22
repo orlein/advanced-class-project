0665960356c4619e318bdf854341dd1a59c55a84
Seyoon Jeong <seyoonagain@gmail.com>
2024-10-23 04:14:53 +0900

    Rename: 페이지 컴포넌트 추가, 이름 변경 및 폴더 이동

--- src/pages/challenges/ChallengesMain.tsx
+++ src/pages/challenges/ChallengesMain.tsx
@@ -1,9 +1,9 @@
 import { Outlet } from 'react-router-dom';
 
-export default function ChallengeMain() {
+export default function ChallengesMain() {
   return (
     <>
-      ChallengeMain
+      ChallengesMain
       <Outlet />
     </>
   );
