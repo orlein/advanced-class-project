8c3456cf955d99412679f27e7a5ade7a46ba6e1d
Seyoon Jeong <seyoonagain@gmail.com>
2024-10-22 21:27:04 +0900

    Feat: 사이드바 구현

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import { ThemeProvider } from "./components/theme-provider";
-import HomePage from "./pages/home";
+import { ThemeProvider } from './components/theme-provider';
+import { SidebarProvider } from './components/ui/sidebar';
+import LeftSideBar from './components/LeftSideBar.tsx';
+import { Outlet } from 'react-router-dom';
 
 export default function App() {
   return (
     <ThemeProvider>
-      <HomePage />
+      <SidebarProvider>
+        <div className='flex h-screen'>
+          <LeftSideBar />
+          <div className='flex-1 p-4'>
+            <Outlet />
+          </div>
+        </div>
+      </SidebarProvider>
     </ThemeProvider>
   );
 }
