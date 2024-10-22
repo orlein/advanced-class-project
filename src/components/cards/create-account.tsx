2d7b7ba34d876e3baaaeaa22fdb838b6ca1d8422
원도훈 <dnjsehgns98@gmail.com>
2024-10-23 00:07:52 +0900

    test

--- src/components/cards/create-account.tsx
+++ src/components/cards/create-account.tsx
@@ -16,9 +16,9 @@ export function DemoCreateAccount() {
   return (
     <Card>
       <CardHeader className="space-y-1">
-        <CardTitle className="text-2xl">Create an account</CardTitle>
+        <CardTitle className="text-2xl">계정 생성</CardTitle>
         <CardDescription>
-          Enter your email below to create your account
+          이메일을 입력하여 계정을 만드세요.
         </CardDescription>
       </CardHeader>
       <CardContent className="grid gap-4">
@@ -47,6 +47,30 @@ export function DemoCreateAccount() {
             </svg>
             Google
           </Button>
+          <Button variant="outline">
+            <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4 mr-2 dark:invert"
+            >
+              <title>Naver</title>
+              <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
+            </svg>
+            Naver
+          </Button>
+          <Button variant="outline">
+            <svg
+                role="img"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-4 w-4 mr-2 dark:invert"
+            >
+              <title>Facebook</title>
+              <path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
+            </svg>
+            Facebook
+          </Button>
         </div>
         <div className="relative">
           <div className="absolute inset-0 flex items-center">
@@ -54,21 +78,21 @@ export function DemoCreateAccount() {
           </div>
           <div className="relative flex justify-center text-xs uppercase">
             <span className="bg-background px-2 text-muted-foreground">
-              Or continue with
+              계속 진행하세요.
             </span>
           </div>
         </div>
         <div className="grid gap-2">
-          <Label htmlFor="email">Email</Label>
+          <Label htmlFor="email">이메일</Label>
           <Input id="email" type="email" placeholder="m@example.com" />
         </div>
         <div className="grid gap-2">
-          <Label htmlFor="password">Password</Label>
+          <Label htmlFor="password">비밀번호</Label>
           <Input id="password" type="password" />
         </div>
       </CardContent>
       <CardFooter>
-        <Button className="w-full">Create account</Button>
+        <Button className="w-full">계정 생성</Button>
       </CardFooter>
     </Card>
   );
