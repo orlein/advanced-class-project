a6172e8a735d2e1428cc9adcf2b82cc468330908
원도훈 <dnjsehgns98@gmail.com>
2024-10-23 18:52:47 +0900

    프로필 페이지 구현

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,28 @@ import { ThemeProvider } from './components/theme-provider';
 import { SidebarProvider } from './components/ui/sidebar';
 import LeftSideBar from './components/LeftSideBar.tsx';
 import { Outlet } from 'react-router-dom';
+import { useState } from 'react';
 
 export default function App() {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(prev => !prev);
+  };
+
   return (
-    <ThemeProvider>
-      <SidebarProvider>
-        <div className='flex h-screen'>
-          <LeftSideBar />
-          <div className='flex-1 p-4'>
-            <Outlet />
+      <ThemeProvider>
+        <SidebarProvider>
+          <div className="flex w-full">
+            <LeftSideBar
+                isSidebarOpen={isSidebarOpen}
+                toggleSidebar={toggleSidebar}
+            />
+            <div className="flex-1 p-4">
+              <Outlet />
+            </div>
           </div>
-        </div>
-      </SidebarProvider>
-    </ThemeProvider>
+        </SidebarProvider>
+      </ThemeProvider>
   );
 }
--- src/pages/account/SignIn.tsx
+++ src/pages/account/SignIn.tsx
@@ -1,3 +1,75 @@
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs"
+
 export default function SignIn() {
-  return <>Sign in</>;
+  return (
+      <Tabs defaultValue="account" className="w-[400px]">
+        <TabsList className="grid w-full grid-cols-2">
+          <TabsTrigger value="account">로그인</TabsTrigger>
+          <TabsTrigger value="password">비밀번호 찾기</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <Card>
+            <CardHeader>
+              <CardTitle>로그인</CardTitle>
+              <CardDescription>
+                아이디와 비밀번호를 입력하여 로그인 하세요.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="name">아이디</Label>
+                <Input id="name" placeholder="아이디를 입력하세요." />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="username">비밀번호</Label>
+                <Input id="username" placeholder="비밀번호를 입력하세요." />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>로그인</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+
+        <TabsContent value="password">
+          <Card>
+            <CardHeader>
+              <CardTitle>비밀번호 찾기</CardTitle>
+              <CardDescription>
+                비밀번호를 변경하세요.
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="new">새로운 비밀번호</Label>
+                <Input id="new" type="password"/>
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="new">새로운 비밀번호 확인</Label>
+                <Input id="new" type="password"/>
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>비밀번호 저장</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+  )
 }
