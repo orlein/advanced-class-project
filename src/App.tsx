0ae84e1d13ae1338ad98753a5d6a3b4194b33da0
원도훈 <dnjsehgns98@gmail.com>
2024-10-26 23:04:44 +0900

    documentation : LeftSideBar

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,9 @@ import { SidebarProvider } from './components/ui/sidebar';
 import LeftSideBar from './components/LeftSideBar.tsx';
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header.tsx';
-import { useState } from 'react';
 import AuthContextProvider from './contexts/AuthContext.tsx';
 
 export default function App() {
-  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
-  const toggleSidebar = () => setIsSidebarOpen((prev) => !prev);
 
   return (
     <AuthContextProvider>
@@ -16,11 +13,8 @@ export default function App() {
         <SidebarProvider>
           <div className='flex w-screen'>
             <Header />
-            <section className={`${isSidebarOpen ? 'w-64' : 'w-16'}`}>
-              <LeftSideBar
-                isSidebarOpen={isSidebarOpen}
-                toggleSidebar={toggleSidebar}
-              />
+            <section className='z-60'>
+              <LeftSideBar />
             </section>
             <section className='flex-1 p-10 mt-16 flex-grow'>
               <Outlet />
