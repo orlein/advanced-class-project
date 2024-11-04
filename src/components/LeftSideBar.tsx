b2569d64ef9c8111296fd9f90bd8e0ece8849aef
원도훈 <dnjsehgns98@gmail.com>
2024-11-04 14:48:59 +0900

    documentation : 비로그인시 테마설정 사이드바 추가

--- src/components/LeftSideBar.tsx
+++ src/components/LeftSideBar.tsx
@@ -28,6 +28,9 @@ import {
   NotebookText,
   SettingsIcon,
   Swords,
+  Sun,
+  Moon,
+  Laptop,
 } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
@@ -36,6 +39,12 @@ import Profile from '../assets/profile.jpg';
 import { useWideScreen } from '@/hooks/use-wideScreen';
 import UserMenuDropdown from './UserMenuDropdown';
 import { DropdownMenu, DropdownMenuTrigger } from './ui/dropdown-menu';
+import {
+  DropdownMenuContent,
+  DropdownMenuRadioGroup,
+  DropdownMenuRadioItem,
+} from './ui/dropdown-menu';
+import { useTheme, Theme } from '@/components/theme-provider';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
 
 const menuItems = [
@@ -95,14 +104,17 @@ export function LeftSideBar() {
   const { open, setOpen, setOpenMobile, isMobile } = useSidebar();
   const [openIndices, setOpenIndices] = useState<boolean[]>([]);
   const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const { theme, setTheme } = useTheme();
+
   const handleIconClick = (index?: number) => {
     if (!open) setOpen(true);
     setOpenIndices((prevState) => {
       const newState = [...prevState];
-      if (index) newState[index] = !newState[index];
+      if (index !== undefined) newState[index] = !newState[index];
       return newState;
     });
   };
+
   const handleMenuClick = (url: string) => {
     navigate(url);
     if (!isMobile) {
@@ -112,113 +124,149 @@ export function LeftSideBar() {
     }
     if (!isWideScreen) setOpenIndices([]);
   };
+
   return (
-    <Sidebar collapsible='icon' className='z-50'>
-      <SidebarHeader className='h-16 justify-center bg-background'>
-        <SidebarMenuItem className='p-0.5'>
-          <SidebarTrigger onClick={() => setOpenIndices([])} />
-        </SidebarMenuItem>
-      </SidebarHeader>
-      <SidebarContent className='bg-background'>
-        {menuItems.map((item, index) => (
-          <SidebarMenuItem key={item.title}>
-            {item.collapsible && (
-              <Collapsible
-                defaultOpen={false}
-                open={openIndices[index] || false}
-                onOpenChange={() => handleIconClick(index)}
-                className='group/collapsible'
-              >
-                <SidebarGroup>
-                  <CollapsibleTrigger>
+      <Sidebar collapsible='icon' className='z-50'>
+        <SidebarHeader className='h-16 justify-center bg-background'>
+          <SidebarMenuItem className='p-0.5'>
+            <SidebarTrigger onClick={() => setOpenIndices([])} />
+          </SidebarMenuItem>
+        </SidebarHeader>
+        <SidebarContent className='bg-background'>
+          {menuItems.map((item, index) => (
+              <SidebarMenuItem key={item.title}>
+                {item.collapsible && (
+                    <Collapsible
+                        defaultOpen={false}
+                        open={openIndices[index] || false}
+                        onOpenChange={() => handleIconClick(index)}
+                        className='group/collapsible'
+                    >
+                      <SidebarGroup>
+                        <CollapsibleTrigger>
+                          <SidebarMenuButton asChild>
+                            <div className='hover:bg-accent hover:text-accent-foreground'>
+                              <item.icon />
+                              <span>{item.title}</span>
+                              <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180' />
+                            </div>
+                          </SidebarMenuButton>
+                        </CollapsibleTrigger>
+                        <CollapsibleContent>
+                          <SidebarMenuSub>
+                            {item.collapsible.map((subItem, subIndex) => (
+                                <SidebarMenuSubItem key={subIndex}>
+                                  <SidebarMenuSubButton asChild>
+                                    <div
+                                        className='block rounded-md px-2 py-1 text-sm hover:bg-accent hover:text-accent-foreground'
+                                        onClick={() => handleMenuClick(subItem.url)}
+                                    >
+                                      {subItem.title}
+                                    </div>
+                                  </SidebarMenuSubButton>
+                                </SidebarMenuSubItem>
+                            ))}
+                          </SidebarMenuSub>
+                        </CollapsibleContent>
+                      </SidebarGroup>
+                    </Collapsible>
+                )}
+                {!item.collapsible && (
+                    <SidebarGroup>
+                      <SidebarMenuButton asChild>
+                        <div
+                            onClick={() => handleMenuClick(item.url)}
+                            className='hover:bg-accent hover:text-accent-foreground'
+                        >
+                          <item.icon />
+                          <span>{item.title}</span>
+                        </div>
+                      </SidebarMenuButton>
+                    </SidebarGroup>
+                )}
+              </SidebarMenuItem>
+          ))}
+        </SidebarContent>
+        <SidebarFooter className='bg-background'>
+          <SidebarMenu>
+            {!user && (
+                <>
+                  {/* 테마 변경 메뉴 */}
+                  <SidebarMenuItem>
+                    <DropdownMenu>
+                      <DropdownMenuTrigger asChild>
+                        <SidebarMenuButton asChild>
+                          <div className='hover:bg-accent hover:text-accent-foreground'>
+                            <Sun />
+                            <span>테마 변경</span>
+                          </div>
+                        </SidebarMenuButton>
+                      </DropdownMenuTrigger>
+                      <DropdownMenuContent side='right' align='start'>
+                        <DropdownMenuRadioGroup
+                            value={theme}
+                            onValueChange={(value) => setTheme(value as Theme)}
+                        >
+                          <DropdownMenuRadioItem value='light'>
+                            <Sun className='mr-2 h-4 w-4' />
+                            <span>라이트 모드</span>
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value='dark'>
+                            <Moon className='mr-2 h-4 w-4' />
+                            <span>다크 모드</span>
+                          </DropdownMenuRadioItem>
+                          <DropdownMenuRadioItem value='system'>
+                            <Laptop className='mr-2 h-4 w-4' />
+                            <span>시스템 설정</span>
+                          </DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                      </DropdownMenuContent>
+                    </DropdownMenu>
+                  </SidebarMenuItem>
+                  {/* 로그인 메뉴 */}
+                  <SidebarMenuItem>
                     <SidebarMenuButton asChild>
-                      <div className='hover:bg-accent hover:text-accent-foreground'>
-                        <item.icon />
-                        <span>{item.title}</span>
-                        <ChevronDown className='ml-auto transition-transform group-data-[state=open]/collapsible:rotate-180' />
+                      <div
+                          onClick={() => handleMenuClick('/sign-in')}
+                          className='hover:bg-accent hover:text-accent-foreground'
+                      >
+                        <LogIn />
+                        <span>로그인</span>
                       </div>
                     </SidebarMenuButton>
-                  </CollapsibleTrigger>
-                  <CollapsibleContent>
-                    <SidebarMenuSub>
-                      {item.collapsible.map((item, itemIndex) => (
-                        <SidebarMenuSubItem key={itemIndex}>
-                          <SidebarMenuSubButton asChild>
-                            <div
-                              className='block rounded-md px-2 py-1 text-sm hover:bg-accent hover:text-accent-foreground'
-                              onClick={() => handleMenuClick(item.url)}
-                            >
-                              {item.title}
-                            </div>
-                          </SidebarMenuSubButton>
-                        </SidebarMenuSubItem>
-                      ))}
-                    </SidebarMenuSub>
-                  </CollapsibleContent>
-                </SidebarGroup>
-              </Collapsible>
+                  </SidebarMenuItem>
+                </>
             )}
-            {!item.collapsible && (
-              <SidebarGroup>
-                <SidebarMenuButton asChild>
-                  <div
-                    onClick={() => handleMenuClick(item.url)}
-                    className='hover:bg-accent hover:text-accent-foreground'
-                  >
-                    <item.icon />
-                    <span>{item.title}</span>
-                  </div>
-                </SidebarMenuButton>
-              </SidebarGroup>
-            )}
-          </SidebarMenuItem>
-        ))}
-      </SidebarContent>
-      <SidebarFooter className='bg-background'>
-        <SidebarMenu>
-          {!user && (
-            <SidebarMenuItem>
-              <SidebarMenuButton asChild>
-                <div
-                  onClick={() => handleMenuClick('/sign-in')}
-                  className='hover:bg-accent hover:text-accent-foreground'
-                >
-                  <LogIn />
-                  <span>로그인</span>
-                </div>
-              </SidebarMenuButton>
-            </SidebarMenuItem>
-          )}
-          {user && (
-            <SidebarMenuItem>
-              <DropdownMenu onOpenChange={setDropdownOpen} open={dropdownOpen}>
-                <SidebarMenuButton
-                  size='lg'
-                  className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
-                  onClick={() => handleIconClick()}
-                >
-                  <Avatar className='h-8 w-8 rounded-lg'>
-                    <AvatarImage src={Profile} alt='profile' />
-                    <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
-                  </Avatar>
-                  <DropdownMenuTrigger asChild>
-                    <div className='flex items-center justify-between w-full'>
-                      <div className='grid flex-1 text-left text-sm leading-tight'>
-                        <span className='truncate font-semibold'>username</span>
-                        <span className='truncate text-xs'>
+            {user && (
+                <SidebarMenuItem>
+                  <DropdownMenu onOpenChange={setDropdownOpen} open={dropdownOpen}>
+                    <SidebarMenuButton
+                        size='lg'
+                        className='data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground'
+                        onClick={() => handleIconClick()}
+                    >
+                      <Avatar className='h-8 w-8 rounded-lg'>
+                        <AvatarImage src={Profile} alt='profile' />
+                        <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
+                      </Avatar>
+                      <DropdownMenuTrigger asChild>
+                        <div className='flex items-center justify-between w-full'>
+                          <div className='grid flex-1 text-left text-sm leading-tight'>
+                            <span className='truncate font-semibold'>username</span>
+                            <span className='truncate text-xs'>
                           username@username.com
                         </span>
-                      </div>
-                      <ChevronsUpDown className='ml-auto size-4' />
-                    </div>
-                  </DropdownMenuTrigger>
-                </SidebarMenuButton>
-                <UserMenuDropdown setDropdownOpen={setDropdownOpen} />
-              </DropdownMenu>
-            </SidebarMenuItem>
-          )}
-        </SidebarMenu>
-      </SidebarFooter>
-    </Sidebar>
+                          </div>
+                          <ChevronsUpDown className='ml-auto size-4' />
+                        </div>
+                      </DropdownMenuTrigger>
+                    </SidebarMenuButton>
+                    <UserMenuDropdown setDropdownOpen={setDropdownOpen} />
+                  </DropdownMenu>
+                </SidebarMenuItem>
+            )}
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
   );
 }
--- src/components/UserMenuDropdown.tsx
+++ src/components/UserMenuDropdown.tsx
@@ -1,105 +1,105 @@
-import { LogOut, User, Moon, Sun, Laptop } from 'lucide-react';
-import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
+import {LogOut, User, Moon, Sun, Laptop} from 'lucide-react';
+import {Avatar, AvatarFallback, AvatarImage} from '@/components/ui/avatar';
 import Profile from '../assets/profile.jpg';
 import {
-  DropdownMenuContent,
-  DropdownMenuGroup,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
-  DropdownMenuItem,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSub,
-  DropdownMenuSubTrigger,
-  DropdownMenuSubContent,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuItem,
+    DropdownMenuRadioGroup,
+    DropdownMenuRadioItem,
+    DropdownMenuSub,
+    DropdownMenuSubTrigger,
+    DropdownMenuSubContent,
 } from './ui/dropdown-menu';
-import { useNavigate } from 'react-router-dom';
-import { useTheme, Theme } from '@/components/theme-provider';
-import { useAuthContext } from '@/contexts/AuthContext';
+import {useNavigate} from 'react-router-dom';
+import {useTheme, Theme} from '@/components/theme-provider';
+import {useAuthContext} from '@/contexts/AuthContext';
 
 const dropdownMenuItems = [
-  { title: '나의 프로필', icon: User, url: 'my-profile', divider: true },
-  { title: '로그아웃', icon: LogOut, url: '/', divider: false },
+    {title: '나의 프로필', icon: User, url: 'my-profile', divider: true},
+    {title: '로그아웃', icon: LogOut, url: '/', divider: false},
 ];
 
 interface SetDropdownOpen {
-  setDropdownOpen: React.Dispatch<React.SetStateAction<boolean>>;
+    setDropdownOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-export default function UserMenuDropdown({ setDropdownOpen }: SetDropdownOpen) {
-  const navigate = useNavigate();
-  const { theme, setTheme } = useTheme();
-  const { signOut } = useAuthContext();
+export default function UserMenuDropdown({setDropdownOpen}: SetDropdownOpen) {
+    const navigate = useNavigate();
+    const {theme, setTheme} = useTheme();
+    const {signOut} = useAuthContext();
 
-  const handleClick = (title: string, url?: string) => {
-    if (title === '로그아웃') {
-      signOut();
-    }
-    if (url) {
-      navigate(url);
-    }
-    setDropdownOpen(false);
-  };
+    const handleClick = (title: string, url?: string) => {
+        if (title === '로그아웃') {
+            signOut();
+        }
+        if (url) {
+            navigate(url);
+        }
+        setDropdownOpen(false);
+    };
 
-  return (
-    <DropdownMenuContent
-      className='w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-l relative bottom-2'
-      side='bottom'
-      align='end'
-      sideOffset={4}
-      aria-label='사용자 메뉴'
-    >
-      <DropdownMenuLabel className='p-0 font-normal'>
-        <div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm h-14'>
-          <Avatar className='h-8 w-8 rounded-lg'>
-            <AvatarImage src={Profile} alt='profile' />
-            <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
-          </Avatar>
-          <div className='grid flex-1 text-left text-sm leading-tight'>
-            <span className='truncate font-semibold'>username</span>
-            <span className='truncate text-xs'>username@username.com</span>
-          </div>
-        </div>
-      </DropdownMenuLabel>
-      <DropdownMenuSeparator />
-      <DropdownMenuGroup>
-        {dropdownMenuItems.map((item) => (
-          <DropdownMenuItem
-            key={item.title}
-            onClick={() => handleClick(item.title, item.url)}
-            className='hover:bg-accent hover:text-accent-foreground'
-          >
-            <item.icon className='mr-2 h-4 w-4' />
-            <span>{item.title}</span>
-          </DropdownMenuItem>
-        ))}
-        {/* 테마 변경 메뉴 추가 */}
-        <DropdownMenuSub>
-          <DropdownMenuSubTrigger>
-            <Sun className='mr-2 h-4 w-4' />
-            <span>테마 변경</span>
-          </DropdownMenuSubTrigger>
-          <DropdownMenuSubContent>
-            <DropdownMenuRadioGroup
-              value={theme}
-              onValueChange={(value) => setTheme(value as Theme)}
-            >
-              <DropdownMenuRadioItem value='light'>
-                <Sun className='mr-2 h-4 w-4' />
-                <span>라이트 모드</span>
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value='dark'>
-                <Moon className='mr-2 h-4 w-4' />
-                <span>다크 모드</span>
-              </DropdownMenuRadioItem>
-              <DropdownMenuRadioItem value='system'>
-                <Laptop className='mr-2 h-4 w-4' />
-                <span>시스템 설정</span>
-              </DropdownMenuRadioItem>
-            </DropdownMenuRadioGroup>
-          </DropdownMenuSubContent>
-        </DropdownMenuSub>
-      </DropdownMenuGroup>
-    </DropdownMenuContent>
-  );
+    return (
+        <DropdownMenuContent
+            className='w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-l relative bottom-2'
+            side='bottom'
+            align='end'
+            sideOffset={4}
+            aria-label='사용자 메뉴'
+        >
+            <DropdownMenuLabel className='p-0 font-normal'>
+                <div className='flex items-center gap-2 px-1 py-1.5 text-left text-sm h-14'>
+                    <Avatar className='h-8 w-8 rounded-lg'>
+                        <AvatarImage src={Profile} alt='profile'/>
+                        <AvatarFallback className='rounded-lg'>CN</AvatarFallback>
+                    </Avatar>
+                    <div className='grid flex-1 text-left text-sm leading-tight'>
+                        <span className='truncate font-semibold'>username</span>
+                        <span className='truncate text-xs'>username@username.com</span>
+                    </div>
+                </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator/>
+            <DropdownMenuGroup>
+                {dropdownMenuItems.map((item) => (
+                    <DropdownMenuItem
+                        key={item.title}
+                        onClick={() => handleClick(item.title, item.url)}
+                        className='hover:bg-accent hover:text-accent-foreground'
+                    >
+                        <item.icon className='mr-2 h-4 w-4'/>
+                        <span>{item.title}</span>
+                    </DropdownMenuItem>
+                ))}
+                {/* 테마 변경 메뉴 추가 */}
+                <DropdownMenuSub>
+                    <DropdownMenuSubTrigger>
+                        <Sun className='mr-2 h-4 w-4'/>
+                        <span>테마 변경</span>
+                    </DropdownMenuSubTrigger>
+                    <DropdownMenuSubContent>
+                        <DropdownMenuRadioGroup
+                            value={theme}
+                            onValueChange={(value) => setTheme(value as Theme)}
+                        >
+                            <DropdownMenuRadioItem value='light'>
+                                <Sun className='mr-2 h-4 w-4'/>
+                                <span>라이트 모드</span>
+                            </DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value='dark'>
+                                <Moon className='mr-2 h-4 w-4'/>
+                                <span>다크 모드</span>
+                            </DropdownMenuRadioItem>
+                            <DropdownMenuRadioItem value='system'>
+                                <Laptop className='mr-2 h-4 w-4'/>
+                                <span>시스템 설정</span>
+                            </DropdownMenuRadioItem>
+                        </DropdownMenuRadioGroup>
+                    </DropdownMenuSubContent>
+                </DropdownMenuSub>
+            </DropdownMenuGroup>
+        </DropdownMenuContent>
+    );
 }
