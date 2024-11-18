a269694dd818c8aa385f0a051520833cac793ba4
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-18 23:45:32 +0900

    Design: 프로필 사진 삭제 ui 추가

--- src/components/molecule/ProfileImage.tsx
+++ src/components/molecule/ProfileImage.tsx
@@ -1,12 +1,17 @@
+import { Dispatch, SetStateAction, useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import { User2 } from 'lucide-react';
 
 const ProfileImage = ({
   url,
   variant,
+  setProfileImage,
+  isEditing,
 }: {
   url: string | undefined;
   variant: 'sidebar' | 'list' | 'post' | 'myProfile' | 'userProfile';
+  setProfileImage?: Dispatch<SetStateAction<string>>;
+  isEditing?: boolean;
 }) => {
   const STYLE =
     variant === 'sidebar'
@@ -18,10 +23,29 @@ const ProfileImage = ({
       : variant === 'myProfile'
       ? 'size-48 rounded-full'
       : 'size-40 rounded-full';
+  const [showDeleteOption, setShowDeleteOption] = useState(false);
   return (
     <Avatar className={STYLE}>
-      <AvatarImage src={url} alt="profile" className="object-cover" />
-      <AvatarFallback className="rounded-full">
+      {variant === 'myProfile' && isEditing && setProfileImage && (
+        <div
+          className="relative"
+          onMouseOver={() => setShowDeleteOption(true)}
+          onMouseLeave={() => setShowDeleteOption(false)}
+        >
+          <AvatarImage src={url} alt="profile" className="object-cover" />
+          {showDeleteOption && (
+            <div className="absolute top-0 left-0 bg-zinc-950 bg-opacity-30 size-48 text-center content-center">
+              <p className="cursor-pointer text-sm font-bold" onClick={() => setProfileImage('')}>
+                삭제
+              </p>
+            </div>
+          )}
+        </div>
+      )}
+      {(variant !== 'myProfile' || !isEditing) && (
+        <AvatarImage src={url} alt="profile" className="object-cover" />
+      )}
+      <AvatarFallback className={STYLE}>
         <User2 className="size-full text-muted-foreground" />
       </AvatarFallback>
     </Avatar>
