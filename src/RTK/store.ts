775de8f63c31d5538df596e36b96834810033021
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-19 14:38:54 +0900

    Fix: 파일 이동 및 api 오류 수정

--- src/RTK/store.ts
+++ src/RTK/store.ts
@@ -1,24 +1,18 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './slice';
-import { postsApi } from '@/features/posts/postsApi';
-import accountApi, { authApi } from '@/api/accountApi';
+import { postsApi } from '@/api/postsApi';
+import accountApi from '@/api/accountApi';
 import imageApi from '@/api/imageApi';
 
 export const store = configureStore({
   reducer: {
     [accountApi.reducerPath]: accountApi.reducer,
     [postsApi.reducerPath]: postsApi.reducer,
-    [authApi.reducerPath]: authApi.reducer,
     [imageApi.reducerPath]: imageApi.reducer,
     auth: authSlice.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(
-      accountApi.middleware,
-      postsApi.middleware,
-      authApi.middleware,
-      imageApi.middleware,
-    ),
+    getDefaultMiddleware().concat(accountApi.middleware, postsApi.middleware, imageApi.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
