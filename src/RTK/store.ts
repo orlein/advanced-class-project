e5af202d216779e0716af42c482ca3eb10cc82e5
Seyoon Jeong <seyoonagain@gmail.com>
2024-11-20 20:07:48 +0900

    Feat: 챌린지 페이지 기본 레이아웃 구현

--- src/RTK/store.ts
+++ src/RTK/store.ts
@@ -3,16 +3,23 @@ import authSlice from './slice';
 import { postsApi } from '@/api/postsApi';
 import accountApi from '@/api/accountApi';
 import imageApi from '@/api/imageApi';
+import challengeApi from '@/api/challengeApi';
 
 export const store = configureStore({
   reducer: {
     [accountApi.reducerPath]: accountApi.reducer,
     [postsApi.reducerPath]: postsApi.reducer,
     [imageApi.reducerPath]: imageApi.reducer,
+    [challengeApi.reducerPath]: challengeApi.reducer,
     auth: authSlice.reducer,
   },
   middleware: getDefaultMiddleware =>
-    getDefaultMiddleware().concat(accountApi.middleware, postsApi.middleware, imageApi.middleware),
+    getDefaultMiddleware().concat(
+      accountApi.middleware,
+      postsApi.middleware,
+      imageApi.middleware,
+      challengeApi.middleware,
+    ),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
--- src/components/molecule/ChallengeCard.tsx
+++ src/components/molecule/ChallengeCard.tsx
@@ -1,20 +1,33 @@
-import { Challenge } from '@/types/challenge';
-import Tag from '../atom/Tag';
+import { Heart } from 'lucide-react';
+import { Card } from '../ui/card';
+import { ChallengeData } from '@/types/challenge';
 
-const ChallengeCard = ({ title, startDate, endDate, tags }: Challenge) => {
+interface CardProps {
+  challenge: ChallengeData;
+}
+
+const ChallengeCard = ({ challenge }: CardProps) => {
+  const calculateRemainingDays = (endDate: string): number => {
+    return Math.ceil((new Date(endDate).getTime() - new Date().getTime()) / (1000 * 60 * 60 * 24));
+  };
   return (
-    <div className="bg-black padding-[10px] flex flex-col gap-5 py-[15px] px-[10px]">
-      <img className="w-[300px] h-[300px]" />
-      <div className="w-[300px] flex flex-col gap-5">
-        <span>{title}</span>
-        <span>{`${startDate} - ${endDate}`}</span>
-        <div className="flex gap-2">
-          {tags.map(tag => (
-            <Tag tag={tag} />
-          ))}
+    <Card
+      key={challenge.id}
+      className="p-5 pb-3 flex flex-col gap-3 w-full aspect-[2/3] max-w-sm justify-between"
+    >
+      <div className="flex flex-col gap-3">
+        <div className="w-full aspect-square border">image</div>
+        <h3 className="text-xl font-bold line-clamp-1">{challenge.title}</h3>
+        <div className="text-sm flex gap-2 items-center">
+          <Heart size={18} />
+          {challenge.likeCount}
         </div>
+        <p className="text-sm line-clamp-2">{challenge.description}</p>
       </div>
-    </div>
+      <p className="text-sm text-end text-muted-foreground">
+        {calculateRemainingDays(challenge.endDate)}일 남음
+      </p>
+    </Card>
   );
 };
 
